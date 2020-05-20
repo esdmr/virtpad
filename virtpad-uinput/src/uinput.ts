e50c40f180a18f4bc25e77fece94c989dffff164
esdmr <esdmr0@gmail.com>
2020-05-20 23:42:42 +0430

    Put that old-virtpad in virtpad-uinput.

--- virtpad-uinput/src/uinput.ts
+++ virtpad-uinput/src/uinput.ts
@@ -4,19 +4,20 @@ import uinput from 'uinput';
 const _uin = bindings('../node_modules/uinput/build/Release/uinput.node');
 const inputEvent = _uin.input_event as uinput.InputEvent;
 
+interface UInputConfig {
+	setup: uinput.SetupConfig;
+	create: uinput.CreateConfig;
+}
+
 export class Input {
 	private uinputCache?: uinput.UInput;
-
-	constructor (
-		readonly setupConfig: uinput.SetupConfig,
-		readonly createConfig: uinput.CreateConfig,
-	) { }
+	constructor (readonly config: UInputConfig) { }
 
 	async init () {
 		if (this.uinputCache == null) {
-			const device = await uinput.setup(this.setupConfig);
+			const device = await uinput.setup(this.config.setup);
 			this.uinputCache = device;
-			await device.create(this.createConfig);
+			await device.create(this.config.create);
 		}
 	}
 
